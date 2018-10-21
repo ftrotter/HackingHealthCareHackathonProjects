9079e84aee76365905a12505373b042c8d584360
Fred Trotter <fred.trotter@gmail.com>
2018-10-21 01:40:03 -0500

    works

--- QRMarkDown.class.php
+++ QRMarkDown.class.php
@@ -41,15 +41,6 @@ class QRMarkDown extends \cebe\markdown\GithubMarkdown{
 
 		$qrCode = new QrCode($url);
 		$qrCode->writeFile($qr_code_save_to_file);
-
-		$qr_link = "
-<div class='card' style='width: 100px;'>
-  <img class='card-img-top' src='$qr_code_file' $alt>
-  <div class='card-body'>
-    <h5 class='card-title'>from $domain</h5>
-  </div>
-</div>
-		";
 		$qr_link = "<b>$text</b> <img width='100px'  $alt src='$qr_code_file'>";
 
 		return($qr_link);
@@ -59,26 +50,40 @@ class QRMarkDown extends \cebe\markdown\GithubMarkdown{
 
 	protected function renderUrl($block)
 	{
-		echo "Running renderUrl\n";
 		$url = htmlspecialchars($block[1], ENT_COMPAT | ENT_HTML401, 'UTF-8');
 		$decodedUrl = urldecode($block[1]);
 		$secureUrlText = preg_match('//u', $decodedUrl) ? $decodedUrl : $block[1];
 		$text = htmlspecialchars($secureUrlText, ENT_NOQUOTES | ENT_SUBSTITUTE, 'UTF-8');
 		$normal_link = "<a href=\"$url\">$text</a>";
 
-		$qr_link = "$text <img src='./fredtrotter.png'>";
+		$md5_url = md5($url); //this will be our qrcode filename
+		$qr_code_file = "qrcode_images/".$md5_url . ".png";
+		$qr_code_save_to_file = __DIR__ . "/html_out/$qr_code_file";;
+
+		$qrCode = new QrCode($url);
+		$qrCode->writeFile($qr_code_save_to_file);
+		$qr_link = "<b>$text</b> <img width='100px'  $alt src='$qr_code_file'>";
 		return($qr_link);
 	}
 
 	protected function renderAutoUrl($block)
 	{
-		echo "Running renderAutoUrl\n";
 		$href = htmlspecialchars($block[1], ENT_COMPAT | ENT_HTML401, 'UTF-8');
 		$decodedUrl = urldecode($block[1]);
 		$secureUrlText = preg_match('//u', $decodedUrl) ? $decodedUrl : $block[1];
 		$text = htmlspecialchars($secureUrlText, ENT_NOQUOTES | ENT_SUBSTITUTE, 'UTF-8');
 		$old_link =  "<a href=\"$href\">$text</a>";
-		$qr_link = "$text <img src='./fredtrotter.png'>";
+
+		$url = $href;
+		$alt = '';
+
+		$md5_url = md5($url); //this will be our qrcode filename
+		$qr_code_file = "qrcode_images/".$md5_url . ".png";
+		$qr_code_save_to_file = __DIR__ . "/html_out/$qr_code_file";;
+
+		$qrCode = new QrCode($url);
+		$qrCode->writeFile($qr_code_save_to_file);
+		$qr_link = "<b>$text</b> <img width='100px'  $alt src='$qr_code_file'>";
 		return($qr_link);
 	}
 
